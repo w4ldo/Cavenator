bf6b188fbe93d5e3a219312b644740cf97e9adae
w4ldo <matias_isosaari@hotmail.com>
2019-10-12 00:55:37 +0300

    week6+

--- Cavenator/src/main/java/cavenator/Main.java
+++ Cavenator/src/main/java/cavenator/Main.java
@@ -16,22 +16,22 @@ public class Main {
      * @param args
      */
     public static void main(String[] args) {
-//        String[][] map = new String[200][300];
-//        CaveGenerator cavegen = new CaveGenerator(map);
-//        GUI gui = new GUI(cavegen);
-//        gui.setVisible(true);
+        String[][] map = new String[200][300];
+        CaveGenerator cavegen = new CaveGenerator(map);
+        GUI gui = new GUI(cavegen);
+        gui.setVisible(true);
 
-        // Manual testing
-        String[][] testMap = new String[1000][1000];
-        CaveGenerator testCavegen = new CaveGenerator(testMap);
-        long initialTime = System.currentTimeMillis();
-        testCavegen.generateCaves(0);
-        long afterGeneration = System.currentTimeMillis();
-        testCavegen.shapeMapA(1, 4);
-        long afterShaping = System.currentTimeMillis();
-        System.out.println("GenerateCave: " + (afterGeneration - initialTime) + "ms");
-        System.out.println("ShapeCave: " + (afterShaping - afterGeneration) + "ms");
-        System.out.println("Total: " + (afterShaping - initialTime) + "ms");
+// Manual testing
+//        String[][] testMap = new String[1000][1000];
+//        CaveGenerator testCavegen = new CaveGenerator(testMap);
+//        long initialTime = System.currentTimeMillis();
+//        testCavegen.generateCaves(0);
+//        long afterGeneration = System.currentTimeMillis();
+//        testCavegen.shapeMapA(1, 4);
+//        long afterShaping = System.currentTimeMillis();
+//        System.out.println("GenerateCave: " + (afterGeneration - initialTime) + "ms");
+//        System.out.println("ShapeCave: " + (afterShaping - afterGeneration) + "ms");
+//        System.out.println("Total: " + (afterShaping - initialTime) + "ms");
 
     }
 
